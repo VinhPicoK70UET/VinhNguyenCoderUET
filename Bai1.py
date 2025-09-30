0b89cf671d7e1a70b1a28254d30d93671e40b158
VinhPicoK70UET <25022042@vnu.edu.vn>
2025-09-30 17:30:39 +0700

    baitap

--- Bai1.py
+++
@@ -1,3 +0,0 @@
-a= int(input("Nhập số n bất kì "))
-daonguoc_a = int(str(a)[::-1])
-print ("Số đảo ngược của a là: ", daonguoc_a)
--- Bai2.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Nhập số n bất kì "))
-b = int(input("Nhập số m bất kì "))
-print ("2 số đầu vào là: ", a, "và", b)
-a = a ^ b
-b = a ^ b
-a = a ^ b
-print ("2 số sau khi hoán đổi là: ", a, "và", b)
