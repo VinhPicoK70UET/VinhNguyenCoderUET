d657bde30001cf3a80ee58e95b62261a08b0440f
VinhPicoK70UET <25022042@vnu.edu.vn>
2025-12-11 19:28:17 +0700

    Create baitaptuan5.py

---
+++ baitaptuan5.py
@@ -0,0 +1,11 @@
+#W5A1:
+a, b = map(int, input("Nhập hai số nguyên a và b: ").split())
+def max_of_two (a,b) -> int:
+    if a > b:
+        return a
+    if b > a:
+        return b
+print("Số giá trị lớn hơn là: ", max_of_two(a,b))
+
+#W5A2:
+
